9859075a16cfae79b2ca84076fd6c8769efce329
Josimar Junior <josimarjunior@Josimars-MacBook-Pro.local>
2026-03-14 13:22:16 -0300

    fix sns subscription

--- post-deployment/sns_replication.tf
+++ post-deployment/sns_replication.tf
@@ -6,6 +6,7 @@ resource "aws_sns_topic_subscription" "primary_to_secondary" {
 }
 
 resource "aws_sns_topic_subscription" "secondary_to_primary" {
+  provider             = aws.secondary
   protocol             = "sqs"
   raw_message_delivery = true
   topic_arn            = data.aws_sns_topic.secondary.arn
